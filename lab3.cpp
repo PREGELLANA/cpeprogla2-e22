3a4f946ca46a4da3bfe79ad9f2c0085dc38784bb
EDELYN PREGELLANA <EVPREGELLANA@fit.edu.ph>
2016-09-10 11:42:00 +0800

    Initial /c/Users/EVPregellana/Desktop/git/cpeprogla2-e22/lab3.cpp

---
+++ lab3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int myStrCmp(char* str1, char* str2){
+	//return strlen str
+	return myStrcmp;
+}
+
+int main(){
+	char str1[100];
+	char str2[100];
+	
+	cout<< myStrCmp(str1, str2);
+}
