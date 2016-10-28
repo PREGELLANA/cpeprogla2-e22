25dc8d77005ad5512032b2c370a6bc947f1c08dc
PREGELLANA <edelynpregellana07@gmail.com>
2016-10-28 16:36:40 +0800

    Create pattern.cpp

---
+++ pattern.cpp
@@ -0,0 +1,20 @@
+//read
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+    int arr[]={1,2,4,3};
+    int x=0;
+	int a, b ,i;
+	
+    ifstream read("input.txt");
+    read >> a >> b;
+    for(i=a;i<=b;i++){
+      cout << i-- <<" ";
+           i+=arr[(x%4)];  
+      x++;
+    }
+    //system("pause");
+    return 0;
+}
